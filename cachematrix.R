b3a9ebea89dd22c1ea9fcdaeaf878e781755480d
sp1r0s <sp1r0s@users.noreply.github.com>
2015-03-14 13:35:49 +0000

    Added my solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates a special matrix that stores its inverse
+## The second function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated the cached inverse is returned
 
-## Write a short comment describing this function
-
+## Returns a list containing four functions to get and set the data and the inverse
 makeCacheMatrix <- function(x = matrix()) {
+  
+  invertedMatrix <- NULL
+  
+  get <- function() { x }
+  set <- function(newX) { x <<- newX ; invertedMatrix <<- NULL }
+  setInvertedMatrix <- function(newInvertedMatrix = matrix()) { invertedMatrix <<- newInvertedMatrix }
+  getInvertedMatrix <- function() { invertedMatrix }
 
+  list(get = get, set = set, getInvertedMatrix = getInvertedMatrix, setInvertedMatrix = setInvertedMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  matrixToBeInverted <- x$get()
+  cachedInvertedMatrix <- x$getInvertedMatrix()
+  
+  ## If the data are different the cachedInvertedMatrix would be NULL
+  ## See set() function in the makeCacheMatrix() function
+  if (!is.null(cachedInvertedMatrix)) {
+    message("getting cached data")
+    return(cachedInvertedMatrix)
+  }
+  
+  invertedMatrix <- solve(matrixToBeInverted)
+  x$setInvertedMatrix(invertedMatrix)
+  
+  return(invertedMatrix)
 }
